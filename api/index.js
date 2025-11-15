b83f275047052e8fbc9c0b8399c5755203aa7b3f
Vishal <vishuSkywalker>
2025-11-15 18:18:03 +0530

    changes to module to require

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from "express";
+const express = require("express");
 const app = express();
+const port = "3000";
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
