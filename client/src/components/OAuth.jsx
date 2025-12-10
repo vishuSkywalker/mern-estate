8ac9535e35bd2882df4796e8cc8ff94f35ceecde
Vishal <vishuSkywalker>
2025-12-10 20:12:44 +0530

    OAuth Changes

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const OAuth = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider()
